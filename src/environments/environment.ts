85cb3294003d35d10e5354a0d1846b06d348723f
Matt Payne <Payne@MattPayne.org>
2018-02-10 14:33:10 -0600

    Attempt deploy with twitter auth on my firebase account

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: false,
     firebaseConfig: {
         apiKey: 'APIKEY',
-        authDomain: 'DEV-APP.firebaseapp.com',
-        databaseURL: 'https://DEV-APP.firebaseio.com',
+        authDomain: 'angular2018q1.firebaseapp.com',
+        databaseURL: 'https://angular2018q1.firebaseio.com/',
         projectId: 'DEV-APP',
         storageBucket: 'DEV-APP.appspot.com',
         messagingSenderId: '123456789'
