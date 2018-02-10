c662136fa0effeb762f74ab77622f5e1bca984ec
Matt Payne <Payne@MattPayne.org>
2018-02-10 15:48:55 -0600

    My firebase app public api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: 'APIKEY',
+        apiKey: 'AIzaSyDTkcRvIE2BDXLl_59t4uH4jYBkuNMpNPY',
         authDomain: 'angular2018q1.firebaseapp.com',
         databaseURL: 'https://angular2018q1.firebaseio.com/',
         projectId: 'DEV-APP',
