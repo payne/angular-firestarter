135dbb49e536768db2d3bf91aaa2a2bf635d5e2c
Matt Payne <Payne@MattPayne.org>
2018-04-16 19:37:04 -0500

    misc

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@ export const environment = {
         apiKey: 'AIzaSyDTkcRvIE2BDXLl_59t4uH4jYBkuNMpNPY',
         authDomain: 'angular2018q1.firebaseapp.com',
         databaseURL: 'https://angular2018q1.firebaseio.com/',
-        projectId: 'DEV-APP',
-        storageBucket: 'DEV-APP.appspot.com',
-        messagingSenderId: '123456789'
-    }
+        projectId: 'angular2018q1',
+        storageBucket: 'angular2018q1.appspot.com',
+        messagingSenderId: '562224226663',
+    },
 };
